d196f376c198c7c50cdd0e80dcfc4f3fafbf9693
Aleksandr A Serdyukov <ASerdyukov@alfabank.ru>
2023-05-19 20:46:54 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {ethers} from 'ethers';
 
 function App() {
+
+  const [userAccount, setUserAccount] =  useState("");
+  const [balance, setBalance] =  useState("0");
+
+    const onConnect = () => {
+        //@ts-ignore
+       if(window.ethereum){
+           //@ts-ignore
+           window.ethereum.request({method: "eth_requestAccounts"}).then((account) => {
+               setUserAccount(account[0])
+               getBalance(account[0])
+           })
+           //@ts-ignore
+           window.ethereum.on("accountChanged", onConnect)
+           //@ts-ignore
+           window.ethereum.on("chainChanged", chainChangedHandler)
+       }else{
+            alert("Установите метамаск")
+       }
+    }
+
+    const chainChangedHandler = () => {
+        window.location.reload()
+    }
+
+    const getBalance = (account: string) => {
+           //@ts-ignore
+            window.ethereum.request({method: "eth_getBalance", params: [account, 'latest']}).then((balance) => {
+                setBalance(ethers.formatEther(balance))
+            })
+
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Подключи кошелек</h2>
+        <button onClick={onConnect}>Подключить кошелек</button>
+        { userAccount ? (
+            <div>
+                <p>User = {userAccount}</p>
+                <p>Balance = {balance}</p>
+            </div>
+        ) : (
+        <span>NotUser</span>
+        )}
+
+
     </div>
   );
 }
